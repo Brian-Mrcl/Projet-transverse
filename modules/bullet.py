ade7b96de30a84524ca71d4da2081d1c8ff190f4
Pandarock0 <151137645+Pandarock0@users.noreply.github.com>
2024-05-09 21:58:55 +0200

    Update_Bullets
    - Change velocity of the bullets and also the picture
    - Add a physique function for diagonal shot

--- modules/bullet.py
+++ modules/bullet.py
@@ -1,9 +1,15 @@
 import pygame
 
+
+def calculate_bullets_position(angle_rad, vitesse_initiale, temps, gravite=-9.81):
+    x = vitesse_initiale * math.cos(angle_rad) * temps
+    y = vitesse_initiale * math.sin(angle_rad) * temps - 0.5 * gravite * temps ** 2
+    return x, y
+
 class Bullet(pygame.sprite.Sprite) :
     def __init__(self, player, shift_map):     #on a récupéré le player grâce au self dans la classe player sur prjectiles
         super().__init__() #super class
-        self.velocity = 0.5
+        self.velocity = 5
         self.player = player
         self.image = pygame.image.load("graphics/projectile.png")
         self.image = pygame.transform.scale(self.image, (50, 50)) #changer la taille du projectile
@@ -16,7 +22,7 @@ class Bullet(pygame.sprite.Sprite) :
 
     def rotate(self):
         #faire tourner le projectile
-        self.angle += 0.35
+        self.angle += 5
         self.image = pygame.transform.rotozoom(self.origin_image, self.angle, 1)
         self.rect = self.image.get_rect(center=self.rect.center) # une rotation par rapport au centre car sinon sautillment car tout les spites sont représenté avec un rectangle
 
